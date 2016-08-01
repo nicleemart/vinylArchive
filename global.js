a5216812316660de5747d3f0cab3f750bd3407fe
Nikki <nicleemart@gmail.com>
2016-08-01 18:34:21 -0500

    can add members

--- global.js
+++ global.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", function (){
 
 	var next = document.getElementById("nextButton");
+	var albumArtistInput = document.getElementById("albumArtist");
+	var artistInfoInput = document.getElementById("artistInfo");
 
 	next.addEventListener("click", function(){
 		var validate = new XMLHttpRequest();
@@ -13,13 +15,16 @@ window.addEventListener("load", function (){
 			var apiResponse = JSON.parse(e.target.responseText);
 			var error = document.getElementById("error");
 
-			var x = document.forms["form"]["album"].value
-			var y = document.forms["form"]["artist"].value;
+			var x = document.forms["albumArtistForm"]["album"].value
+			var y = document.forms["albumArtistForm"]["artist"].value;
 
 			for (i = 0; i < apiResponse.blah.albums.length; i++){
 				if (apiResponse.blah.albums[i].albumTitle == x && y == apiResponse.blah.artist){
 				error.style.display = "block";
-				return false;
+				}
+				else {
+					albumArtistInput.style.display = "none";
+					artistInfoInput.style.display = "block";
 				}
 			}
 
