6961f36d559670071baf6d395ffbf6b67e4268fa
Nikki <nicleemart@gmail.com>
2016-08-04 13:37:36 -0500

    trying to hide div when there is more than one on page

--- global.js
+++ global.js
@@ -66,13 +66,14 @@ window.addEventListener("load", function() {
     addMember.addEventListener("click", function() {
 
         artistInfo.style.display = "none";
-        // var n = member.length;
-        // member[n-1].style.display = "none";
-        // instrument[n-1].style.display = "none";
 
         var addMemberDiv = document.createElement("addAdditionalMember");
-        addMemberDiv.innerHTML = "<input type='text' name='name' placeholder='Name' class='members2'><br><input type='text' name='instruments2' placeholder='Instrument(s)' class='instruments'><br>";
+        addMemberDiv.className = "addMember";
+        addMemberDiv.innerHTML = "<input type='text' name='name' placeholder='Name'><br><input type='text' name='instruments' placeholder='Instrument(s)'><br>";
         document.getElementById("wrapper").appendChild(addMemberDiv);
+
+        var n = addMemberDiv.length;
+        addMemberDiv[n-1].style.display = "none";
     });
 
 });
